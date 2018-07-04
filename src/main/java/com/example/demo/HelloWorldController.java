e5c2f8c1f860d46b7ff6995a64a2a635beb0d6c0
Devender <devender.rawat@comprinno.net>
2018-07-05 00:45:12 +0530

    V-1

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,19 +1,14 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.Map;
+
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.sun.glass.ui.SystemClipboard;
-import jdk.nashorn.internal.ir.annotations.Reference;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.Past;
 
+//HelloWorldController class will publish all web service end points
+// Handles requests for the Greeting
 
 @Controller
 public class HelloWorldController {
@@ -21,13 +16,7 @@ public class HelloWorldController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value="/hello-world", method = RequestMethod.POST, produces = { "application/json","application/xml"})
-    @ResponseBody
-    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-        Greeting s=new Greeting(counter.incrementAndGet(), String.format(template, name));
-        System.out.println(s.getName());
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+
 
     @RequestMapping (value = "/op", method = RequestMethod.POST )
     public @ResponseBody Greeting acceptRequest(@RequestBody Greeting gre) throws Exception{
