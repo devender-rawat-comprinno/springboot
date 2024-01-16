53a0f90e5a4e43c9c26e205caa7217254f2da745
devender-rawat-comprinno <40837498+devender-rawat-comprinno@users.noreply.github.com>
2024-01-16 11:45:41 +0530

    Update HelloWorldApp.java

--- src/main/java/com/example/demo/HelloWorldApp.java
+++ src/main/java/com/example/demo/HelloWorldApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApp {
 
     public static void main(String[] args) {
+        System.out.println("running sprint boot");
         SpringApplication.run(HelloWorldApp.class, args);
     }
 
